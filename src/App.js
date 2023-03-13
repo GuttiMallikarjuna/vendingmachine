0222004b8e7c560efcce6f0ec82eef305dfdaecc
GuttiMallikarjuna <78456871+GuttiMallikarjuna@users.noreply.github.com>
2023-03-13 18:33:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,167 +1,73 @@
-import React, { useState } from 'react';
+import React from "react";
+import Products from './products'
+import Product from './product';
 import './App.css';
-import coke from '../src/images/coke1.webp'
-import sprite from '../src/images/sprite.jpg'
-import thumsup from '../src/images/thumsup.jfif'
-import kinley from '../src/images/kinley.jfif'
-import lays1 from '../src/images/lays1.jfif'
-import lays2 from '../src/images/lays2.jfif'
-import lays3 from '../src/images/lays3.jfif'
-import lays4 from '../src/images/lays4.jfif'
-import buiscuit1 from '../src/images/biscuit1.jfif'
-import buiscuit2 from '../src/images/biscuit2.jfif'
-import buiscuit3 from '../src/images/biscuit3.jfif'
-import buiscuit4 from '../src/images/biscuit4.jfif'
 
 
+const App1=()=>{
 
+   const submitClick=()=>{
+    const dd = document.getElementById('dd');
+    const ddItem=dd.options[dd.selectedIndex].text;
+    const item=document.getElementById('item').value;
+    const money=document.getElementById('money').value;
+    const qty=document.getElementById('quantity').value;
+    // eslint-disable-next-line array-callback-return
+    Products.map(product => {
+      const sum= product.price * qty;
+      const remainingbalance= money - sum;
+     
 
-const App=() => {
+        if(product.productName === ddItem && product.id.toString() === item && sum <= money){
+          document.getElementById('message').innerHTML= `<span style="color:green">Remaining Balance is: ${remainingbalance}</span>`;
+              document.getElementById('display').innerHTML= 
+               ` <img src=${product.productImage}/> <br/> Hii take your product.Enjoy the day`;
+              
+           }else if(product.productName === ddItem && product.id.toString() === item && sum > money){
+            document.getElementById('message').innerHTML= `<span style="color:red">Insufficient funds</span>`;
+           }
+   })
+    
+   }
+   
+    return(
+        <>
+        <h3 id="head">Vending Machine</h3>
+        <div className="maindiv">
+        <div className="shop">
+           
+            <div className="products">
+                {Products.map((product)=>(
+                    <Product data={product} />
+                ))}
+            </div>
 
-  const [balance ,setBalance]=useState("");
-  const message=()=>{
-    alert('First add money');
-  }
-  const handleChange=(e)=>{
-    setBalance(e.target.value);
-  }
-  const submitClick=()=>{
-    document.getElementById('balance').innerHTML=`Balance is: ${balance}`
-  }
- 
-  return (   
-     <>
-     <div className='maindiv'>
-     <div className='container'>
-     <div className='card'>
-      <div className='card-body'>
-        <img src={coke} alt="coke"/>
-      </div>
-      <div className='card-footer'>
-        <button onClick={message}>&#8377; 50</button>
+        </div>
+        <div className='subdiv'>
+            <label id="selectlabel">Select product</label>
+            <select id="dd">
+                {
+                    Products.map((product) =>(
+                        <option value={product.id}>{product.productName}</option>
+                    ))
+                }
+            </select><br/>
+      <label>Enter Id</label><br/>
+      <input type="number" id="item"  placeholder='Enter Id here'/><br/>
+      <label>Place Money</label><br/>
+      <input type="number" id="money"  placeholder='Place Money here'/><br/>
+      <label>Quantity</label><br/>
+      <input type="number" id="quantity"  placeholder='Select quantity'/><br/>
+      <button onClick={submitClick}>Enter</button>
+      <p id="message"></p>
+      <div id="display">
+       
       </div>
      </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={sprite} alt="coke" />
-      </div>
-      <div className='card-footer'>
-      
-        <button onClick={message}>&#8377; 40</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={thumsup} alt="coke"/>
-      </div>
-      <div className='card-footer'>
+     
         
-        <button onClick={message}>&#8377; 60</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={kinley} alt="coke" />
-      </div>
-      <div className='card-footer'>
-        
-        <button onClick={message}>&#8377; 20</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={lays1} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 30</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={lays2} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 40</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={lays3} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 30</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={lays4} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 30</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={buiscuit1} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 30</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={buiscuit2} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 10</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={buiscuit3} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 40</button>
-      </div>
-     </div>
-
-     <div className='card'>
-      <div className='card-body'>
-        <img src={buiscuit4} alt="coke" />
-      </div>
-      <div className='card-footer'>
-       
-        <button onClick={message}>&#8377; 25</button>
-      </div>
-     </div>
-     </div>
-     <div className='subdiv'>
-      <label>Add Money</label><br/>
-      <input type="number" value={balance} onChange={handleChange} placeholder='Add money here'/><br/>
-      <button onClick={submitClick}>Add</button>
-      <p id="balance"></p>
-
-     </div>
-     </div>
-
-     </>
-  );
+        </div>
+        </>
+    )
 }
-export default App;
+export default App1;
